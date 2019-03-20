414d490d68b7decc0c0f3c64ebb3ff8b6d9c2fe5
M Miller <miller.2676@osu.edu>
2019-03-20 16:42:00 -0700

    Progressive examples for a Toggle component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+/* eslint-disable react/prefer-stateless-function */
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Toggle from './ToggleRPC';
 
 class App extends Component {
   render() {
@@ -8,17 +11,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Toggle>
+            {({ on, toggle }) => (
+              <div>
+                {on && <h1>Show Me</h1>}
+                <button type="button" onClick={toggle}>Show/Hide</button>
+              </div>
+            )}
+          </Toggle>
         </header>
       </div>
     );
