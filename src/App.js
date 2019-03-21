a0a31b049faf583c5368b45500c220bf8072a526
M Miller <miller.2676@osu.edu>
2019-03-20 17:10:29 -0700

    experiment with Portals

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Toggle from './ToggleRPC';
+import Portal from './Portal';
 
 class App extends Component {
   render() {
@@ -11,15 +12,19 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Toggle>
-            {({ on, toggle }) => (
-              <Fragment>
-                {on && <h1>Show Me</h1>}
-                <button type="button" onClick={toggle}>Show/Hide</button>
-              </Fragment>
-            )}
-          </Toggle>
         </header>
+        <Toggle>
+          {({ on, toggle }) => (
+            <Fragment>
+              {on && <h1>Show Me</h1>}
+              <button type="button" onClick={toggle}>Show/Hide</button>
+              <Portal>
+                {on && <h1>Hi, I am in a portal!</h1>}
+              </Portal>
+            </Fragment>
+          )}
+        </Toggle>
+
       </div>
     );
   }
